ebe7b8721387f74b53af9ed7cffedc669ee187b1
Jorge <jlimajorge@gmail.com>
2025-11-05 13:27:47 -0300

    feta: validaçãoes feitas

--- app/javascript/controllers/parsley_controller.js
+++ app/javascript/controllers/parsley_controller.js
@@ -1,10 +1,19 @@
 // controllers/parsley_controller.js
 import { Controller } from "@hotwired/stimulus"
 import "parsleyjs"
+import "parsleyjs/dist/i18n/pt-br"
 import "../pages/parsley"
 
 export default class extends Controller {
   connect() {
+    window.Parsley.setLocale("pt-br")
     $(this.element).parsley()
+  }
+  
+  disconnect() {
+    // Destroi a instância para evitar duplicação após navegação Turbo
+    if ($(this.element).data("Parsley")) {
+      $(this.element).parsley().destroy()
+    }
   }
 }
